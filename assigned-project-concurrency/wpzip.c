61b12fc3d55d5f509509dabcb277f9f40f5252f2
Emmanuel Ola <eoola@wpi.edu>
2023-04-25 04:04:34 +0000

    Add parallelism code

--- assigned-project-concurrency/wpzip.c
+++ assigned-project-concurrency/wpzip.c
@@ -1,34 +1,112 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <string.h>
 
-struct ThreadInfo {
-    int num;
-    char result[50]; // might need to make this bigger, based on test cases
+#define CHUNK_SIZE 1024
+
+char uncompressed_data[CHUNK_SIZE];
+int uncompressed_size = 0;
+
+struct CompressedChunk
+{
+    int count;
+    char value;
 };
 
-void* zip(void* arg) {
-    //havent put zip code in yet, but this is what accepts the portion of the file its passed, then zips it.
-    //at this point, the zipped file is given to result.
-    pthread_exit(NULL);
+struct CompressedChunk compressed_data[CHUNK_SIZE];
+int compressed_size = 0;
+
+void *compress_chunk(void *arg)
+{
+    int *chunk_index = (int *)arg;
+
+    int i;
+    char current_char = uncompressed_data[(*chunk_index) * CHUNK_SIZE];
+    int count = 1;
+    for (i = 1; i < CHUNK_SIZE; i++)
+    {
+        char next_char = uncompressed_data[(*chunk_index) * CHUNK_SIZE + i];
+        if (next_char == current_char)
+        {
+            count++;
+        }
+        else
+        {
+            compressed_data[(*chunk_index) * CHUNK_SIZE + compressed_size].count = count;
+            compressed_data[(*chunk_index) * CHUNK_SIZE + compressed_size].value = current_char;
+            compressed_size++;
+            current_char = next_char;
+            count = 1;
+        }
+    }
+
+    compressed_data[(*chunk_index) * CHUNK_SIZE + compressed_size].count = count;
+    compressed_data[(*chunk_index) * CHUNK_SIZE + compressed_size].value = current_char;
+    compressed_size++;
+
+    return NULL;
 }
 
-int main() {
-    pthread_t id[2]; //create two threads for now, so we know it works.
-    char file; //at this point still need to add in the command line reader, so this is a placeholder for the file.
-    char final[200] = ""; //This variable should be where the combined result is.
-    for(int i = 0; i < 2; i++){ //again, the two will be replaced by the number of threads later
-        pthread_create(&id[i], NULL, zip, file); //create each thread
-    }
-    //Making sure all threads finish.
-    for(int i = 0; i < 2; i++){
-        pthread_join(id[i], NULL);
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        printf("wzip: numOfThreads file0 [file1...]");
+        return 1;
     }
 
-    for(int i = 0; i < 2; i++){
-        strcat(final, id[i].result); //I broke something with pointers here, but this should put the file back together.
+    FILE *input_file = fopen(argv[2], "rb");
+    if (input_file == NULL)
+    {
+        printf("Error: could not open file %s\n", argv[1]);
+        return 1;
     }
 
-    //Finally, whatever is needed to output in the way the test wants.
+    int num_chunks = 1;
+    fseek(input_file, 0, SEEK_END);
+    int file_size = ftell(input_file);
+    fseek(input_file, 0, SEEK_SET);
+    if (file_size > CHUNK_SIZE)
+    {
+        num_chunks = file_size / CHUNK_SIZE + (file_size % CHUNK_SIZE != 0);
+    }
+    printf("Compressing file '%s' with %d threads and chunk size %d...\n", argv[1], num_chunks, CHUNK_SIZE);
+
+    pthread_t threads[num_chunks];
+    int thread_args[num_chunks];
+
+    int i;
+    for (i = 0; i < num_chunks; i++)
+    {
+        if (i == num_chunks - 1)
+        {
+            uncompressed_size = file_size % CHUNK_SIZE;
+            if (uncompressed_size == 0)
+            {
+                uncompressed_size = CHUNK_SIZE;
+            }
+        }
+        else
+        {
+            uncompressed_size = CHUNK_SIZE;
+        }
+        fread(uncompressed_data, 1, uncompressed_size, input_file);
+        compressed_size = 0;
+        thread_args[i] = i;
+        pthread_create(&threads[i], NULL, compress_chunk, &thread_args[i]);
+    }
+
+    for (i = 0; i < num_chunks; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    for (i = 0; i < compressed_size * num_chunks; i++)
+    {
+        fwrite(&compressed_data[i].count, sizeof(int), 1, stdout);
+        fwrite(&compressed_data[i].value, sizeof(char), 1, stdout);
+    }
+
+    fclose(input_file);
+
     return 0;
 }
